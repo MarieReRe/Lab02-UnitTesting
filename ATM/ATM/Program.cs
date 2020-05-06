95340ad32422807fba5dade1c6a02b05bef023f7
Marie Antons <chefantons@gmail.com>
2020-05-05 20:05:17 -0500

    added welcome message

--- ATM/ATM/Program.cs
+++ ATM/ATM/Program.cs
@@ -6,7 +6,15 @@ namespace ATM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to DeltaV ATM");
+            AtmControls();
+
+        }
+        static decimal balance = 1000;
+        public static void AtmControls()
+        {
+            //This will be the contol
+            Console.WriteLine("Test");
         }
     }
 }
