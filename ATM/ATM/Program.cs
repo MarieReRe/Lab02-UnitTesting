c193fe6310cfd3856b9ba992af08efd568b0d11c
Marie Antons <chefantons@gmail.com>
2020-05-05 20:17:51 -0500

    added user controls, unusable so far

--- ATM/ATM/Program.cs
+++ ATM/ATM/Program.cs
@@ -6,7 +6,30 @@ namespace ATM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to DeltaV ATM");
+            AtmControls();
+
+        }
+        //set initial balance for user 
+        static decimal balance = 1000;
+        public static void AtmControls()
+        {
+            string[] userActionSelection =
+            {
+                "1. View My Balance",
+                "2. Deposit Money",
+                "3. Withdraw Money",
+                "4. Exit"
+
+            };
+            //This will be the contol
+            Console.WriteLine("Welcome to DeltaV ATM, Please choose a number to proceed.");
+
+            //iterate through each user selection
+            foreach(string action in userActionSelection)
+            {
+                Console.WriteLine(action);
+            }
         }
     }
 }
